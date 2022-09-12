5763235369bca603d830fa95cd26621222fb4af3
wowzamarriam <mariumjuw123@gmail.com>
2022-09-12 21:54:29 +0500

    Code

---
+++ android/app/src/main/kotlin/com/example/pakkstan/pakkstan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pakkstan.pakkstan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:pakkstan/Constants/global_variables.dart';
+import 'package:pakkstan/common/widgets/bottom_bar.dart';
+import 'package:pakkstan/features/admin/screens/admin_screen.dart';
+import 'package:pakkstan/features/auth/screens/auth_screen.dart';
+import 'package:pakkstan/features/auth/services/auth_service.dart';
+import 'package:pakkstan/providers/user_provider.dart';
+import 'package:pakkstan/router.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+    ),
+  ], child: const MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    authService.getUserData(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+        useMaterial3: true, // can remove this line
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+          ? Provider.of<UserProvider>(context).user.type == 'user'
+              ? const BottomBar()
+              : const AdminScreen()
+          : const AuthScreen(),
+    );
+  }
+}
